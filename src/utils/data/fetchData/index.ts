c801d11d12cc1e8cd7a392f35e646af78e091b41
Kenneth Tegrado <tegradok@gmail.com>
2022-07-16 19:18:21 +0800

    refactor: improve implementation of fetchData util function

--- src/utils/data/fetchData/index.ts
+++ src/utils/data/fetchData/index.ts
@@ -1,13 +1,8 @@
-interface FetchDataResponse<Type> {
-    status: 200;
-    data: Type[];
-}
-
 const fetchData = async <Type>(
     data: Type[],
     page: number,
     searchParams = ''
-): Promise<FetchDataResponse<Type>> => {
+): Promise<Type[]> => {
     const filteredData = data.filter((item) =>
         Object.values(item).some((nestedItem) =>
             (nestedItem + '').toLowerCase().includes(searchParams.toLowerCase())
@@ -16,7 +11,7 @@ const fetchData = async <Type>(
 
     const paginatedData = filteredData.slice((page - 1) * 10, (page - 1) * 10);
 
-    return { status: 200, data: paginatedData };
+    return paginatedData;
 };
 
 export default fetchData;
