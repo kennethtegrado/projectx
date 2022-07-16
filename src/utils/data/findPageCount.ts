c817b18664ad34178459a299b636d4d252b47046
Kenneth Tegrado <tegradok@gmail.com>
2022-07-16 19:09:25 +0800

    refactor: change file structure for the database util function

--- src/utils/data/findPageCount.ts
+++
@@ -1,4 +0,0 @@
-const findPageCount = async <Type>(data: Type[]): Promise<number> => {
-    return Math.round(data.length);
-};
-export default findPageCount;
---
+++ src/utils/data/findPageCount/index.ts
@@ -0,0 +1,13 @@
+const findPageCount = async <Type>(
+    data: Type[],
+    searchParams = ''
+): Promise<number> => {
+    const filteredData = data.filter((item) =>
+        Object.values(item).some((nestedItem) =>
+            (nestedItem + '').toLowerCase().includes(searchParams.toLowerCase())
+        )
+    );
+
+    return Math.round(filteredData.length);
+};
+export default findPageCount;
